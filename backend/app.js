ed3992f87b091f61c823659be42df35ef6bfc2bd
swet chaudhary <chaudharyswet0@gmail.com>
2026-05-29 08:29:05 +0530

    intial commit

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const dotenv=require('dotenv')
+dotenv.config()
+
+const app=express();
+
+ app.get('/',(req,res)=>{
+    res.send('welcome');
+})
+
+module.exports=app;
